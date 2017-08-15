5f0d18c1cb69c474bb4c7c1d716e123dbb351912
Nick Fitzgerald <fitzgen@gmail.com>
2017-08-15 11:07:58 -0700

    Use bindgen from build.rs
    Instead of hoping that the bindings created from headers on my machine happen to
    be correct, generate them at build time from build.rs.

---
+++ build.rs
@@ -0,0 +1,45 @@
+extern crate bindgen;
+
+use std::env;
+use std::path::PathBuf;
+
+fn main() {
+    if cfg!(target_os = "linux") {
+        generate_linux_bindings();
+    } else if cfg!(target_os = "macos") {
+        generate_macos_bindings();
+    } else {
+        panic!("`findshlibs` does not support the target OS :(");
+    }
+}
+
+fn generate_linux_bindings() {
+    let bindings = bindgen::Builder::default()
+        .header("./src/linux/bindings.h")
+        .whitelisted_function("dl_iterate_phdr")
+        .whitelisted_type(r#"Elf\d*.*"#)
+        .whitelisted_var("PT_.*")
+        .generate()
+        .expect("Should generate linux FFI bindings OK");
+
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings.write_to_file(out_path.join("linux_bindings.rs"))
+        .expect("Should write linux_bindings.rs OK");
+}
+
+fn generate_macos_bindings() {
+    let bindings = bindgen::Builder::default()
+        .header("./src/macos/bindings.h")
+        .whitelisted_function("_dyld_.*")
+        .whitelisted_type("mach_header.*")
+        .whitelisted_type("load_command.*")
+        .whitelisted_type("segment_command.*")
+        .whitelisted_var("MH_MAGIC.*")
+        .whitelisted_var("LC_SEGMENT.*")
+        .generate()
+        .expect("Should generate macOS FFI bindings OK");
+
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings.write_to_file(out_path.join("macos_bindings.rs"))
+        .expect("Should write macos_bindings.rs OK");
+}
