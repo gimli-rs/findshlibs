28032959f8582237f9b60edbe96167e0134b8c47
Nick Fitzgerald <fitzgen@gmail.com>
2017-08-17 10:52:36 -0700

    Add the list-segments example from the docs as an actual example program

--- ci/script.sh
+++ ci/script.sh
@@ -5,7 +5,7 @@ set -eux
 # We always want backtraces for everything.
 export RUST_BACKTRACE=1
 
-cargo build $PROFILE
+cargo build --examples $PROFILE
 cargo test $PROFILE
 
 if [[ "$PROFILE" == "--release" ]]; then
---
+++ examples/list_segments.rs
@@ -0,0 +1,14 @@
+extern crate findshlibs;
+use findshlibs::{Segment, SharedLibrary, TargetSharedLibrary};
+
+fn main() {
+    TargetSharedLibrary::each(|shlib| {
+        println!("{}", shlib.name().to_string_lossy());
+
+        for seg in shlib.segments() {
+            println!("    {}: segment {}",
+                     seg.actual_virtual_memory_address(shlib),
+                     seg.name().to_string_lossy());
+        }
+    });
+}
