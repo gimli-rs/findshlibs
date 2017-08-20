b9af993843dbda74e1d1220daca89ba4c3ab1a1d
Nick Fitzgerald <fitzgen@gmail.com>
2017-08-20 14:19:27 -0700

    Add a test for panicking inside `SharedLibrary::each`

--- src/lib.rs
+++ src/lib.rs
@@ -268,3 +268,21 @@ impl From<()> for IterationControl {
         IterationControl::Continue
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn panic_in_each() {
+        use std::panic;
+
+        match panic::catch_unwind(|| { TargetSharedLibrary::each(|_| panic!("uh oh")); }) {
+            Ok(()) => panic!("Expected a panic, but didn't get one"),
+            Err(any) => {
+                assert!(any.is::<&'static str>(), "panic value should be a &'static str");
+                assert_eq!(*any.downcast_ref::<&'static str>().unwrap(), "uh oh");
+            }
+        }
+    }
+}
