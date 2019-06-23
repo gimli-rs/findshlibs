1ad969d2394f97e80d00af880f60b642f2362c8c
Armin Ronacher <armin.ronacher@active-4.com>
2019-06-23 16:49:20 +0200

    Library::stated_virtual_memory_address -> Library::load_addr

--- src/lib.rs
+++ src/lib.rs
@@ -89,6 +89,7 @@ extern crate libc;
 use std::ffi::CStr;
 use std::fmt::{self, Debug};
 use std::ptr;
+use std::usize;
 
 pub mod unsupported;
 
@@ -319,17 +320,23 @@ pub trait SharedLibrary: Sized + Debug {
     /// Returns the address of where the library is loaded.
     ///
     /// This typically is the stated virtual memory address of the first
-    /// code segment.
-    fn stated_virtual_memory_address(&self) -> Svma {
+    /// code segment but not always.  We follow the breakpad semantics for
+    /// what is considered the load address here so that this information
+    /// can be used to drive server side symbolication systems which are
+    /// generally working with the breakpad definition of what the image
+    /// load address is.
+    fn load_addr(&self) -> Svma {
         self.segments()
             .find(|x| x.is_code())
             .map(|x| x.stated_virtual_memory_address())
-            .unwrap_or_else(|| Svma(ptr::null()))
+            .unwrap_or_else(|| Svma(usize::MAX as _))
     }
 
     /// Returns the size of the image.
     ///
-    /// This typically is the size of the executable code segment.
+    /// This typically is the size of the executable code segment.  This is
+    /// normally used by server side symbolication systems to determine when
+    /// an IP no longer falls into an image.
     fn len(&self) -> usize {
         self.segments()
             .skip_while(|x| !x.is_code())
