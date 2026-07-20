c2a9a34dab397e5c5a2639118f298d87ac1e5265
Nick Fitzgerald <fitzgen@gmail.com>
2026-07-20 12:14:56 -0700

    Fix testing and compilation

--- src/lib.rs
+++ src/lib.rs
@@ -470,7 +470,7 @@ mod tests {
         use std::panic;
 
         match panic::catch_unwind(|| {
-            TargetSharedLibrary::each(|_| panic!("uh oh"));
+            TargetSharedLibrary::each::<_, ()>(|_| panic!("uh oh"));
         }) {
             Ok(()) => panic!("Expected a panic, but didn't get one"),
             Err(any) => {
